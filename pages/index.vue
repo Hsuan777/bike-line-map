444c592d2da001df5fc80bcd50f57de6741c2c7c
Vic <Vic@gmail.com>
2021-11-16 17:44:24 +0800

    [ add ] 自行車站點座標加入圖資

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,40 @@
               尋找附近 U-Bike
             </h2>
             <div class="p-5 bg-white rounded">
+              <div class="row g-2 mb-5">
+                <div class="col-md-6">
+                  <select
+                    class="form-select py-3 border-secondary"
+                    aria-label="Default select example"
+                    @change="changeCity($event.target.value)"
+                  >
+                    <option :selected="nowCityName === '請選擇城市'">
+                      請選擇城市
+                    </option>
+                    <option
+                      v-for="(item, index) in cities"
+                      :key="item.name + index"
+                      :value="item.name"
+                      :selected="item.name === nowCityName"
+                    >
+                      {{ item.name }}
+                    </option>
+                  </select>
+                </div>
+                <div class="col-md-6">
+                  <input
+                    v-model="cacheSearch"
+                    class="form-control text-secondary py-3 border-secondary"
+                    placeholder="請輸入站點名稱"
+                  >
+                </div>
+              </div>
+
               <div class="d-flex align-items-center">
                 <button
                   type="button"
                   class="btn btn-lg btn-info me-5 d-flex align-items-center"
-                  @click="getUserPosition"
+                  @click="setLocation"
                 >
                   <span class="material-icons-outlined text-white me-3"> person_pin_circle </span>
                   以我所在的位置搜尋
@@ -89,7 +118,7 @@
           </ul>
         </div>
         <div class="col-md-10">
-          <div id="mapid" class="magic-height-400" />
+          <div id="mapID" ref="mapID" class="magic-height-400" />
         </div>
       </div>
     </div>
@@ -97,12 +126,107 @@
 </template>
 
 <script>
+import JsSHA from 'jssha';
+
 export default {
   data() {
     return {
       locationUser: {},
       leaflet: {},
       bikeData: [],
+      availabilityBikeData: [],
+      nowCityName: '',
+      cacheSearch: '',
+      cities: [
+        {
+          name: '臺北市',
+          nameEn: 'Taipei',
+          coordinate: {
+            latitude: 25.0462,
+            longitude: 121.5174,
+          },
+        },
+        {
+          name: '新北市',
+          nameEn: 'NewTaipei',
+          coordinate: {
+            latitude: 25.0143926,
+            longitude: 121.4639425,
+          },
+        },
+        {
+          name: '桃園市',
+          nameEn: 'Taoyuan',
+          coordinate: {
+            latitude: 24.989306,
+            longitude: 121.3133955,
+          },
+        },
+        {
+          name: '臺中市',
+          nameEn: 'Taichung',
+          coordinate: {
+            latitude: 24.136941,
+            longitude: 120.685056,
+          },
+        },
+        {
+          name: '臺南市',
+          nameEn: 'Tainan',
+          coordinate: {
+            latitude: 22.997212,
+            longitude: 120.212319,
+          },
+        },
+        {
+          name: '高雄市',
+          nameEn: 'Kaohsiung',
+          coordinate: {
+            latitude: 22.6384542,
+            longitude: 120.3019452,
+          },
+        },
+        {
+          name: '新竹市',
+          nameEn: 'Hsinchu',
+          coordinate: {
+            latitude: 24.801841,
+            longitude: 120.9715962,
+          },
+        },
+        {
+          name: '苗栗縣',
+          nameEn: 'MiaoliCounty',
+          coordinate: {
+            latitude: 24.5699868,
+            longitude: 120.8223152,
+          },
+        },
+        {
+          name: '嘉義市',
+          nameEn: 'Chiayi',
+          coordinate: {
+            latitude: 23.4791004,
+            longitude: 120.4413128,
+          },
+        },
+        {
+          name: '屏東縣',
+          nameEn: 'PingtungCounty',
+          coordinate: {
+            latitude: 22.669248,
+            longitude: 120.4861926,
+          },
+        },
+        {
+          name: '金門縣',
+          nameEn: 'KinmenCounty',
+          coordinate: {
+            latitude: 24.409293,
+            longitude: 118.297844,
+          },
+        },
+      ],
     };
   },
   created() {
@@ -112,7 +236,7 @@ export default {
   },
   mounted() {
     this.getUserPosition();
-    this.getBikeData();
+    this.getBikeData('Taipei');
   },
   methods: {
     getUserPosition() {
@@ -125,10 +249,11 @@ export default {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
       };
-      this.myMap = this.leaflet.map('mapid', {
-        center: [this.locationUser.latitude, this.locationUser.longitude],
-        zoom: 13,
+      this.myMap = this.leaflet.map('mapID', {
+        center: [position.coords.latitude, position.coords.longitude],
+        zoom: 14,
       });
+      // 加入圖層
       this.leaflet
         .tileLayer(
           'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
@@ -144,21 +269,102 @@ export default {
           },
         )
         .addTo(this.myMap);
+      // 顯示使用者座標方圓區域
       this.leaflet
         .circle([this.locationUser.latitude, this.locationUser.longitude], { radius: 3000 })
+        .addTo(this.myMap);
+      // 標示使用者座標
+      this.leaflet
+        .marker([this.locationUser.latitude, this.locationUser.longitude], {
+          icon: this.setIconColor('blue'),
+        })
         .addTo(this.myMap);
     },
     error() {
       alert('無法取得你的位置');
     },
-    getBikeData() {
-      const apiUrl = 'https://ptx.transportdata.tw/MOTC/v2/Bike/Station/Taoyuan?$format=JSON';
-      this.$axios.get(apiUrl).then((res) => {
-        this.bikeData = res.data;
-        this.renderBikeData();
+    getBikeData(city) {
+      const apiUrl = `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/${city}?$format=JSON`;
+      const availabilityApiUrl = `https://ptx.transportdata.tw/MOTC/v2/Bike/Availability/${city}?$format=JSON`;
+      this.$axios
+        .get(apiUrl, {
+          headers: this.getAuthorizationHeader(),
+        })
+        .then((res) => {
+          this.bikeData = res.data;
+        });
+      this.$axios
+        .get(availabilityApiUrl, {
+          headers: this.getAuthorizationHeader(),
+        })
+        .then((res) => {
+          this.availabilityBikeData = res.data;
+          this.bikeData.forEach((item, index) => {
+            this.availabilityBikeData.forEach((availabilityItem) => {
+              if (item.StationUID === availabilityItem.StationUID) {
+                this.bikeData[index] = { ...item, ...availabilityItem };
+              }
+            });
+          });
+          this.renderBikeData();
+        });
+    },
+    setIconColor(color) {
+      const iconColor = new this.leaflet.Icon({
+        iconUrl: `https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-${color}.png`,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      });
+      return iconColor;
+    },
+    renderBikeData() {
+      const markerCluster = require('leaflet.markercluster');
+      this.leaflet = { ...this.leaflet, ...markerCluster };
+      const markers = this.leaflet.markerClusterGroup().addTo(this.myMap);
+      this.bikeData.forEach((item) => {
+        let iconColor = '';
+        if (item.ServiceStatus === 1 && item.AvailableRentBikes > 0) {
+          iconColor = 'green';
+        } else if (item.ServiceStatus === 1 && item.AvailableRentBikes === 0) {
+          iconColor = 'yellow';
+        } else if (item.ServiceStatus === 1 && item.AvailableReturnBikes === 0) {
+          iconColor = 'red';
+        } else {
+          iconColor = 'black';
+        }
+        markers.addLayer();
+        this.leaflet
+          .marker([item.StationPosition.PositionLat, item.StationPosition.PositionLon], {
+            icon: this.setIconColor(iconColor),
+          })
+          .addTo(this.myMap).bindPopup(`<p>${item.StationName.Zh_tw}</p>
+          <p>可借數量 ${item.AvailableRentBikes}</p>
+          <p>歸還車位數量 ${item.AvailableReturnBikes}</p>`);
       });
     },
-    renderBikeData() {},
+    getAuthorizationHeader() {
+      const AppID = '3209d3c409014e8cb42b5e83f861c102';
+      const AppKey = 'qd4_Nh2b30-edGu3vXmbDzaWTFU';
+      const GMTString = new Date().toGMTString();
+      const ShaObj = new JsSHA('SHA-1', 'TEXT');
+      ShaObj.setHMACKey(AppKey, 'TEXT');
+      ShaObj.update(`x-date: ${GMTString}`);
+      const HMAC = ShaObj.getHMAC('B64');
+      const Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+      return { Authorization, 'X-Date': GMTString };
+    },
+    setLocation() {
+      this.nowCityName = '請選擇城市';
+      this.myMap.setView([this.locationUser.latitude, this.locationUser.longitude], 14);
+    },
+    changeCity(value) {
+      this.nowCityName = value;
+      const newCity = this.cities.filter((item) => item.name === value)[0];
+      this.myMap.setView([newCity.coordinate.latitude, newCity.coordinate.longitude], 13);
+      this.getBikeData(newCity.nameEn);
+    },
   },
 };
 </script>
