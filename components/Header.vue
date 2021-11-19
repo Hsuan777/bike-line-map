0ebf2b1f23c4f4a8a02df29c8857534b23fe3c7e
Vic <Vic@gmail.com>
2021-11-19 17:16:39 +0800

    [ edit ] Navbar 排版

--- components/Header.vue
+++ components/Header.vue
@@ -6,31 +6,18 @@
           <img src="~/assets/icon/bike-line-logo.svg" alt="bike-line-log">
         </NuxtLink>
       </h1>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon" />
-      </button>
-      <div id="navbarSupportedContent" class="collapse navbar-collapse">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <NuxtLink to="/" class="nav-link">
-              尋找 U-bike
-            </NuxtLink>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="/recommend" class="nav-link">
-              搜尋路線
-            </NuxtLink>
-          </li>
-        </ul>
-      </div>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <NuxtLink to="/" class="nav-link">
+            尋找 U-bike
+          </NuxtLink>
+        </li>
+        <li class="nav-item">
+          <NuxtLink to="/recommend" class="nav-link">
+            搜尋路線
+          </NuxtLink>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/bike-line-map/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
       { rel: 'preconnect', href: 'https://fonts.gstatic.com'},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700;900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,700&display=swap'},
